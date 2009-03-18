4c771a0ba63fbe1ecc43201ed95a898067bede31
Harut <arutdag@mail.ru>
2009-03-18 11:51:02 +0300

    Docstrings, bugfixes

--- cachecontrol/__init__.py
+++ cachecontrol/__init__.py
@@ -6,11 +6,18 @@ from django.core.cache import cache
 from django.utils.functional import curry
 
 def glue_cache(name, args):
-    x = str(':'.join([name] + map(lambda x: str(x), args)))
+    '''
+    Returns cache name by value and args.
+    '''
+    x = u':'.join([name] + map(lambda x: str(x), args))
     #print "  ", args, [x]
     return x
 
 def cache_tags(name, args):
+    '''
+    Returns all tags to cache key by value and args.
+    The first tag is key's timestamp
+    '''
     tags = [glue_cache('cc_time_tag', [name] + args)]
     for i, arg in enumerate(args):
         tag = glue_cache('cc_time_tag', [name, 'arg', i, arg])
@@ -18,6 +25,10 @@ def cache_tags(name, args):
     return tags
 
 def view_set_cache(name, expire_time=None, args=[], cache_func=lambda: None):
+    '''
+    Returns cache value if exists
+    Otherwise calls cache_funcs, sets cache value to it and returns it.
+    '''
     value = get_cache(name, args)
     if value is None:
         value = cache_func()
@@ -25,49 +36,58 @@ def view_set_cache(name, expire_time=None, args=[], cache_func=lambda: None):
     return value
 
 def clear_cache(name, args):
-#    print "Clearing cache %s" % name
+    '''
+    Clears cache and all of it's tags (if number of args > 1)
+    '''
     if len(args) > 1:
         tags = cache_tags(name, args)
         for tag in tags:
-#            print "   Deleted key %s" % tag
-            #cache.set(tag, datetime.now())
             cache.delete(tag)
     name = glue_cache(name, args)
     cache.delete(name)
 
 def set_cache(name, value, expire_time, args):
-    print "Setting cache %s" % name
+    '''
+    Sets cache value according cache name and "vary_on" args.
+    Sets tag timestamps if they are not set (if number of args > 1)
+    Sets cache key timestamp (if number of args > 1)
+    '''
+#    print "Setting cache %s" % name
     if len(args) > 1:
         time = datetime.now()
         tags = cache_tags(name, args)
-        for tag in tags:
-            cache.set(tag, time, expire_time)
+        cache.set(tags[0], time, expire_time)
+        for tag in tags[1:]:
+            # KADAVR!
+            if not cache.get(tag):
+                cache.set(tag, time, expire_time)
     cache_key = glue_cache(name, args)
- #   print "   Cache key is %s" % cache_key
     cache.set(cache_key, value, expire_time)
 
 def get_cache(name, args):
-    print "Getting cache %s" % name
+    '''
+    Gets cache value according cache name and "vary_on" args
+    If one of cache tags is timed out or lost, returns None
+    '''
+#    print "Getting cache %s" % name
     cache_key = glue_cache(name, args)
-#    print "   Cache key is %s" % cache_key
     result = cache.get(cache_key)
     
     if len(args) > 1 and result is not None:
-        print "   There are few arguments on cache %s" % name
+#        print "   There are few arguments on cache %s" % name
         tags = cache_tags(name, args)
         time = cache.get(tags[0])
         if time:
             for tag in tags[1:]:
                 c = cache.get(tag)
+
                 if not c or c > time:
                     result = None
                     break
-                else:
-                    print "   Tag %s is OK: %s %s" % (tag, c, type(c))
+#                else:
+#                    print "   Tag %s is OK: %s %s" % (tag, c, type(c))
         else:
             result = None
-                
-#    print u"   Cache result is: %s" % unicode(result)[:20]
     return result
 
 class AlreadyRegistered(Exception):
@@ -76,6 +96,9 @@ class DeprecatedName(Exception):
     pass
 
 def _clear_cached(name, args_func, *args, **kwargs):
+    '''
+    Model's save and delete callback
+    '''
     obj = kwargs['instance']
     if registry._registry[name] == 0:
         cache.delete(name)
@@ -88,12 +111,14 @@ def _clear_cached(name, args_func, *args, **kwargs):
         cache.delete(tag)
 
 class CacheRegistry(object):
-
+    '''
+    Stores all registered caches
+    '''
     def __init__(self):
         self._registry = {}
 
     def register(self, name, num_args, model_pairs):
-        name = str(name)
+        #name = str(name)
         if name in self._registry:
             raise AlreadyRegistered(u'name %s already registered.' % name)
         if ':' in name or '|' in name:
